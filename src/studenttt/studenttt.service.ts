5670c96a6786acf88a17de5077b265037f1f7443
Grace100320 <90703351+Grace100320@users.noreply.github.com>
2025-01-10 15:57:29 +0800

    create student

--- src/studenttt/studenttt.service.ts
+++ src/studenttt/studenttt.service.ts
@@ -10,11 +10,6 @@ export class StudentService {
         private studentRepository: Repository<Student>,
     ) { }
 
-    // CREATE
-    async createStudent(student: Partial<Student>): Promise<Student> {
-        const newStudent = this.studentRepository.create(student);
-        return await this.studentRepository.save(newStudent);
-    }
 
     // READ (GET ALL)
     async getAllStudents(): Promise<Student[]> {
@@ -28,28 +23,5 @@ export class StudentService {
             throw new NotFoundException('Student not found');
         }
         return student;
-    }
-
-    // UPDATE (PUT)
-    async updateStudent(id: number, updatedData: Partial<Student>): Promise<Student> {
-        const student = await this.getStudentById(id);
-        Object.assign(student, updatedData);
-        return this.studentRepository.save(student);
-    }
-
-    // PARTIAL UPDATE (PATCH)
-    async partialUpdateStudent(id: number, partialData: Partial<Student>): Promise<Student> {
-        const student = await this.getStudentById(id);
-        Object.assign(student, partialData);
-        return this.studentRepository.save(student);
-    }
-
-    // DELETE
-    async deleteStudent(id: number): Promise<{ message: string }> {
-        const result = await this.studentRepository.delete(id);
-        if (result.affected === 0) {
-            throw new NotFoundException('Student not found');
-        }
-        return { message: `The student with ID ${id} has been successfully removed.` };
     }
 }
