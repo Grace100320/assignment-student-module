ac7392d50ebe7dbc03a1af30865f846713df6f89
Grace100320 <90703351+Grace100320@users.noreply.github.com>
2025-01-10 15:34:54 +0800

    create-students

--- src/studenttt/studenttt.controller.ts
+++ src/studenttt/studenttt.controller.ts
@@ -11,28 +11,4 @@ export class StudentController {
         return this.studentService.createStudent(student);
     }
 
-    @Get()
-    findAll(): Promise<Student[]> {
-        return this.studentService.getAllStudents();
-    }
-
-    @Get(':id')
-    findOne(@Param('id') id: number): Promise<Student> {
-        return this.studentService.getStudentById(id);
-    }
-
-    @Put(':id')
-    update(@Param('id') id: number, @Body() student: Partial<Student>) {
-        return this.studentService.updateStudent(id, student);
-    }
-
-    @Patch(':id')
-    partialupdate(@Param('id') id: number, @Body() student: Partial<Student>): Promise<Student> {
-        return this.studentService.updateStudent(id, student);
-    }
-
-    @Delete(':id')
-    remove(@Param('id') id: number) {
-        return this.studentService.deleteStudent(id);
-    }
 }
